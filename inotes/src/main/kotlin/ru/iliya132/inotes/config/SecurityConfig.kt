5e82877aeaaca3de363ba55b97359e2ca9d20c3e
Илья <iliya132@live.com>
2022-06-16 08:13:22 +0300

    configured some logging and cors

--- inotes/src/main/kotlin/ru/iliya132/inotes/config/SecurityConfig.kt
+++ inotes/src/main/kotlin/ru/iliya132/inotes/config/SecurityConfig.kt
@@ -23,83 +23,83 @@ import ru.iliya132.inotes.services.security.UserDetailsService
 @EnableWebSecurity
 class SecurityConfig {
 
-	@Autowired
-	lateinit var userDetailsService: UserDetailsService
+    @Autowired
+    lateinit var userDetailsService: UserDetailsService
 
-	@Autowired
-	@Throws(Exception::class)
-	fun configureGlobal(auth: AuthenticationManagerBuilder) {
-		auth.userDetailsService(userDetailsService)
-	}
+    @Autowired
+    @Throws(Exception::class)
+    fun configureGlobal(auth: AuthenticationManagerBuilder) {
+        auth.userDetailsService(userDetailsService)
+    }
 
-	@Bean
-	fun passwordEncoder(): PasswordEncoder {
-		return BCryptPasswordEncoder()
-	}
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
 
-	@Bean
-	fun appAuthSuccessHandler(): AppAuthSuccessHandler? {
-		return AppAuthSuccessHandler()
-	}
+    @Bean
+    fun appAuthSuccessHandler(): AppAuthSuccessHandler? {
+        return AppAuthSuccessHandler()
+    }
 
-	@Bean
-	fun appAuthFailureHandler(): AppAuthFailHandler {
-		return AppAuthFailHandler()
-	}
+    @Bean
+    fun appAuthFailureHandler(): AppAuthFailHandler {
+        return AppAuthFailHandler()
+    }
 
-	@Bean
-	fun appLogoutSuccessHandler(): LogoutSuccessHandler {
-		return AppLogoutSuccessHandler()
-	}
+    @Bean
+    fun appLogoutSuccessHandler(): LogoutSuccessHandler {
+        return AppLogoutSuccessHandler()
+    }
 
-	@Bean
-	fun authEntryPoint(): AuthenticationEntryPoint {
-		return AuthEntryPoint()
-	}
+    @Bean
+    fun authEntryPoint(): AuthenticationEntryPoint {
+        return AuthEntryPoint()
+    }
 
-	@Bean
-	fun filterChain(http: HttpSecurity): SecurityFilterChain {
-		http.csrf().disable()
-				.cors()
-				.and()
-				.authorizeRequests()
-				.antMatchers("/api*").hasRole("user")
-				.antMatchers("/auth/login*").permitAll()
-				.antMatchers("/auth/register*").permitAll()
-				.anyRequest().authenticated()
-				.and()
-				.formLogin()
-				.loginProcessingUrl("/auth/login")
-				.successHandler(appAuthSuccessHandler())
-				.failureHandler(appAuthFailureHandler())
-				.and()
-				.logout().logoutUrl("/auth/logout").permitAll().logoutSuccessHandler(appLogoutSuccessHandler())
-				.invalidateHttpSession(true)
-				.deleteCookies("JSESSIONID", "SESSION")
-				.and()
-				.exceptionHandling()
-				.authenticationEntryPoint(authEntryPoint())
-				.and()
-				.sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-                
-		setupStaticHeaders(http)
-		return http.build()
-	}
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.csrf().disable()
+            .cors()
+            .and()
+            .authorizeRequests()
+            .antMatchers("/api*").hasRole("user")
+            .antMatchers("/auth/login*").permitAll()
+            .antMatchers("/auth/register*").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin()
+            .loginProcessingUrl("/auth/login")
+            .successHandler(appAuthSuccessHandler())
+            .failureHandler(appAuthFailureHandler())
+            .and()
+            .logout().logoutUrl("/auth/logout").permitAll().logoutSuccessHandler(appLogoutSuccessHandler())
+            .invalidateHttpSession(true)
+            .deleteCookies("JSESSIONID", "SESSION")
+            .and()
+            .exceptionHandling()
+            .authenticationEntryPoint(authEntryPoint())
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
 
-	private fun setupStaticHeaders(http: HttpSecurity) {
-		addHeader(http, "Access-Control-Allow-Origin", "http://localhost:3000")
-		addHeader(http, "Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT")
-		addHeader(http, "Access-Control-Max-Age", "3600")
-		addHeader(
-				http, "Access-Control-Allow-Headers", "Authorization, Content-Type, Accept, " +
-				"x-requested-with, Cache-Control"
-		         )
-		addHeader(http, "Access-Control-Allow-Credentials", "true")
-	}
+        setupStaticHeaders(http)
+        return http.build()
+    }
 
-	private fun addHeader(http: HttpSecurity, header: String, value: String) {
-		http.headers().addHeaderWriter(StaticHeadersWriter(header, value))
-	}
+    private fun setupStaticHeaders(http: HttpSecurity) {
+        addHeader(http, "Access-Control-Allow-Origin", "https://www.i-note.online")
+        addHeader(http, "Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT")
+        addHeader(http, "Access-Control-Max-Age", "3600")
+        addHeader(
+            http, "Access-Control-Allow-Headers", "Authorization, Content-Type, Accept, " +
+                    "x-requested-with, Cache-Control"
+        )
+        addHeader(http, "Access-Control-Allow-Credentials", "true")
+    }
+
+    private fun addHeader(http: HttpSecurity, header: String, value: String) {
+        http.headers().addHeaderWriter(StaticHeadersWriter(header, value))
+    }
 
 }
