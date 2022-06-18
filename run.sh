8799947be08f01bc3963d88750ac51de30f3956c
Илья <iliya132@live.com>
2022-06-18 23:42:56 +0300

    added script to build and run project on server

--- run.sh
+++ run.sh
@@ -1,15 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 #cleanup
-pkill java
-docker stop i_note
-
 cd /home/iliya132/inotes/INotes
+echo "stopping backend"
+pkill java
+echo "stopping docker container"
+docker stop i_note
+docker rm i_note
+
+#fetch
+echo "GIT: checkout main branch"
 git checkout main
+echo "GIT: pulling changes"
 git pull
+
+#build & run backend
 cd inotes
+echo "MVN: building"
 mvn install
+echo "starting backend"
 nohup java -jar -Dspring.profiles.active=production  ./target/inotes-0.0.1-SNAPSHOT.jar > /dev/null 2>%1 &
-cd ../frontend
-npm ci
+
+#build & run frontend
+echo "installing node_modules"
+cd /home/iliya132/inotes/INotes/frontend
+echo "building docker image"
 docker build -t i-note .
+echo "running docker container"
 docker run --name i_note -p 3000:3000 -d i-note
