1735289bfe979fc674e27507d69119333eef932b
Илья <iliya132@live.com>
2022-06-19 15:21:28 +0300

    confirm leaving when context changed

--- frontend/src/components/Note/types.ts
+++ frontend/src/components/Note/types.ts
@@ -3,6 +3,6 @@ import { INote } from "../../store/types";
 
 export interface INoteProps{
     note: INote;
-    onClick?: (note: INote) => void;
+    onClick?: () => void;
     isSelected?: boolean;
 }
