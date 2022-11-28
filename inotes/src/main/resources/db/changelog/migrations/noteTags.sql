10e5fd21914b0fe73895702e17e42ee8d2a0b5ca
Ilya132 <iliya132@live.com>
2022-11-28 09:45:51 +0300

    INote-fixit fix for deleteing note with tag in it

--- inotes/src/main/resources/db/changelog/migrations/noteTags.sql
+++ inotes/src/main/resources/db/changelog/migrations/noteTags.sql
@@ -8,3 +8,10 @@ create table if not exists note_tags(
 );
 
 create index if not exists idx_note_id on note_tags (note_id);
+
+--changeset iliya132:fix-for-deleting-notes
+begin;
+alter table note_tags
+drop constraint note_tags_note_id_fkey,
+add constraint note_tags_note_id_fkey foreign key (note_id) references notes(id)
+    on delete cascade
