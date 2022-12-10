54f7d058273f1f219f2dcc6d493da64e1df0fd48
iliya132 <iliya132@live.com>
2022-12-10 15:56:46 +0300

    fix for backend start

--- run.sh
+++ run.sh
@@ -5,46 +5,50 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-#cleanup
-cd /home/iliya132/inotes/INotes
-
 #fetch
 echo "GIT: checkout main branch"
 git checkout main
 echo "GIT: pulling changes"
 git pull
+echo "Done"
 
 #Nginx section - required sudo access
 echo "updating nginx"
 pkill nginx
 cp ./nginx.conf /etc/nginx/nginx.conf
 nginx
+echo "Done"
 
 #build backend
-cd inotes
+cd /home/iliya132/inotes/INotes/inotes
 echo "Build backend"
 mvn install -Dmaven.test.skip #cause i got 1 VM and it speeds build a lot (all tests run at GitHub before merging)
 echo "Done"
 #build frontend
 cd /home/iliya132/inotes/INotes/frontend
-echo "Build frontend"
+
 mkdir /home/iliya132/inotes/INotes/frontend/sslcert
 cp /etc/letsencrypt/live/i-note.online/fullchain.pem /home/iliya132/inotes/INotes/frontend/sslcert/fullchain.pem
 cp /etc/letsencrypt/live/i-note.online/privkey.pem /home/iliya132/inotes/INotes/frontend/sslcert/privkey.pem
+
+echo "stopping frontend container"
 docker stop i_note
 docker rm i_note
+echo "Done"
+echo "Build frontend container"
 docker build -t i-note .
 echo "Done"
-echo "Stop old backend"
+echo "Stop active backend"
 pkill java
 echo "Done"
 echo "Start new backend"
+cd /home/iliya132/inotes/INotes/inotes
 nohup java -jar -Dspring.profiles.active=production  ./target/inotes-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
 echo "Done"
 echo "Start frontend"
-echo "stopping docker container"
-
 docker run --name i_note -p 3000:3000 -p 3443:3443 -d i-note
+echo "Done"
+echo "cleanup"
 docker image prune -a -f
-
 rm -rf /home/iliya132/inotes/INotes/frontend/sslcert
+echo "Done"
